096312e4359e622c115f1bf1e6ba4b646e342839
coffeecloudgit <168399738+coffeecloudgit@users.noreply.github.com>
2025-04-09 10:24:31 +0800

    Update bandwidth-server.sh

--- bandwidth-server.sh
+++ bandwidth-server.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-# 检查 iperf 是否已经安装
-if ! command -v iperf &> /dev/null
+# 检查 iperf3 是否已经安装
+if ! command -v iperf3 &> /dev/null
 then
-    echo "iperf is not installed. Starting installation..."
+    echo "iperf3 is not installed. Starting installation..."
     if [ -f /etc/debian_version ]; then
         # Debian/Ubuntu 系统
         sudo apt-get update
-        sudo apt-get install -y iperf
+        sudo apt-get install -y iperf3
     elif [ -f /etc/redhat-release ]; then
         # CentOS/RHEL 系统
-        sudo yum install -y iperf
+        sudo yum install -y iperf3
     else
-        echo "Unsupported operating system. Please install iperf manually."
+        echo "Unsupported operating system. Please install iperf3 manually."
         exit 1
     fi
 fi
 
-# 启动 iperf 服务端
-iperf -s    
+# 启动 iperf3 服务端
+iperf3 -s    
